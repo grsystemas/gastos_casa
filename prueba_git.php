e6033121f814e380b3481b1244eb1a778d1dfc06
gustavo <grsystemas@gmail.com>
2013-01-14 18:43:01 -0300

    Esta modificación la estoy haciendo desde php storm directamente

--- prueba_git.php
+++ prueba_git.php
@@ -1,6 +1,7 @@
 <?php
 echo 'esto es solo una prueba';
 echo 'esta es otra prueba';
+	//esta modificación es la tercera
 //ahora modifico por 1era vez para git (que seria la 2da)
 //ahora lo modifico por 2da vez
 //ahora va la 3era
